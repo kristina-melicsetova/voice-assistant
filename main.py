418afa0df11e319016d15b87b8638780ef5b03fd
kristina-melicsetova <70933166+kristina-melicsetova@users.noreply.github.com>
2022-07-04 12:45:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello world')
+import torch
+import sounddevice as sd
+import time
+
+
+language = 'ru'
+model_id = 'ru_v3'
+sample_rate = 48000
+speaker = 'baya'
+put_accent = True
+put_yo = True
+device = torch.device('cpu')
+text = "hello"
