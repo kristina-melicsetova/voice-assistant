acc5f31af311a697d18ff1e7ab912cce7a26264b
kristina-melicsetova <70933166+kristina-melicsetova@users.noreply.github.com>
2022-07-04 12:45:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ put_accent = True
 put_yo = True
 device = torch.device('cpu')
 text = "hello"
+model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models',
+                          model='silero_tts',
+                          language=language,
+                          speaker=model_id)
+
+model.to(device)
